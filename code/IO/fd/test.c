41732304601898e493e51d39d8780a5cfa9d0a0b
GG-Bruse <BJY811@outlook.com>
2023-01-28 23:43:38 +0800

    fd

---
+++ code/IO/fd/test.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+int main()
+{
+    int fd1 = open("./log1.txt",O_RDWR | O_CREAT | O_TRUNC,0666);
+    int fd2 = open("./log2.txt",O_RDWR | O_CREAT | O_TRUNC,0666);
+    int fd3 = open("./log3.txt",O_RDWR | O_CREAT | O_TRUNC,0666);
+    int fd4 = open("./log4.txt",O_RDWR | O_CREAT | O_TRUNC,0666);
+    int fd5 = open("./log5.txt",O_RDWR | O_CREAT | O_TRUNC,0666);
+    printf("fd1: %d\n",fd1);
+    printf("fd2: %d\n",fd2);
+    printf("fd3: %d\n",fd3);
+    printf("fd4: %d\n",fd4);
+    printf("fd5: %d\n",fd5);
+    close(fd1);
+    close(fd2);
+    close(fd3);
+    close(fd4);
+    close(fd5);
+    return 0;
+}
